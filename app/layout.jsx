87ea9271819818039d9067337bb38c1a78964007
Nahuel Santillan <nahueltecnicomdp@gmail.com>
2022-11-24 02:01:44 -0300

    asd

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,21 +1,16 @@
-import '@styles/globals.css'
-import { UserProvider } from '../context/context'
+import './globals.css'
 import Aside from '@components/Aside'
 import Footer from '@components/Footer'
 
 export default function RootLayout({ children }) {
   return (
     <html>
-      <head>
-        <title>Coda</title>
-      </head>
+      <head/>
       <body>
         <div className="coda">
-          <UserProvider>
-            <Aside />
+          <Aside />
             {children}
-            <Footer />
-          </UserProvider>
+          <Footer />
         </div>
       </body>
     </html>
